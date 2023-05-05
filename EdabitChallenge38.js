396a3aadf1b52b225f6a91463ab6f43da4bce328
amit0804-tech <118940994+amit0804-tech@users.noreply.github.com>
2023-05-05 11:33:29 +0530

    Update EdabitChallenge38.js

--- EdabitChallenge38.js
+++ EdabitChallenge38.js
@@ -1,5 +1,5 @@
 // Given an array of scrabble tiles, create a function that outputs the maximum possible score a player can achieve by summing up the total number of points for all the tiles in their hand. Each hand contains 7 scrabble tiles.
-
+var tileHand = [  { score: 1 },  { score: 2 },  { score: 3 },  { score: 4 }];
 function maximumScore(tileHand) {
 	var score = 0;
 	for (i = 0; i < tileHand.length; i++){
